051672204c5d5b6166c8529a5a1c448797aa5b9b
= <=>
2022-06-02 20:40:11 -0600

    add extended user and recipient models

--- gift_app/models.py
+++ gift_app/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from gift_assist import settings
+
+RELATIONSHIP = (
+    "Spouse",
+    "Parent" "Sibling",
+    "Friend",
+    "Grandparent",
+    "Cousin",
+    "Coworker",
+    "Add Relationship",
+)
+
+GENDER = (
+    "Select Gender",
+    "Female",
+    "Male",
+    "Non-Binary",
+)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -6,3 +24,18 @@ class UserProfile(models.Model):
         on_delete=models.CASCADE,
     )
     profile_image = models.ImageField
+
+
+class Recipient(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.Date
+    email = models.EmailField
+    relationship = models.CharField(
+        max_length=16, choices=RELATIONSHIP, default="Spouse"
+    )
+    gender = models.CharField(max_length=10, choices=GENDER, default="Select Gender")
+    notes = models.TextField(max_length=256)
+    user_id = models.ForeignKey(
+        "UserProfile",
+        on_delete=models.SET_NULL,
+    )
