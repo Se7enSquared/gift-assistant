86ed3fb4a588718505ec0a79fe21a921ec2e5fc0
Heather Gray <heathergray153@gmail.com>
2022-06-03 19:58:50 -0600

    add recipient views

--- gift_app/views.py
+++ gift_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 
+from .models import Recipient
+from .forms import RecipientForm
+
+
+def recipient_list(request):
+    pass
+
+
+def recipient_detail(request):
+    pass
+
+
+def recipient_new(request):
+    pass
+
+
+def recipient_edit(request):
+    pass
+
+
+def recipient_delete(request):
+    pass
