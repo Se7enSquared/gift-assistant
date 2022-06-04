9197f34cc501feecf25e24fa349ba560af4d1f5a
Heather Gray <heathergray153@gmail.com>
2022-06-04 00:27:10 -0600

    fix route to be more specific

--- gift_assist/urls.py
+++ gift_assist/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('recipient.urls')),
+    path('', include('gift_app.urls')),
     path('gift-backend/', admin.site.urls),
 ]
