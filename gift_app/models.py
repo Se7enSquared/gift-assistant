e5d3c2e52ce13ce89c5e85528e09611e42875ca4
Heather Gray <heathergray153@gmail.com>
2022-06-04 00:23:27 -0600

    clean up choices and make them correct

--- gift_app/models.py
+++ gift_app/models.py
@@ -17,30 +17,31 @@ class Profile(models.Model):
 class Recipient(models.Model):
 
     RELATIONSHIP = (
-        (0, "Select Relationship"),
-        (1, "Spouse"),
-        (2, "Parent"),
-        (3, "Sibling"),
-        (4, "Friend"),
-        (5, "Grandparent"),
-        (6, "Cousin"),
-        (7, "Coworker"),
-        (1111, "Add Relationship"),
+        ("Select", "Select"),
+        ("Spouse", "Spouse"),
+        ("Parent", "Parent"),
+        ("Sibling", "Sibling"),
+        ("Friend", "Friend"),
+        ("Grandparent", "Grandparent"),
+        ("Cousin", "Cousin"),
+        ("Coworker", "Coworker"),
+        ("Other", "Other")
     )
 
     GENDER = (
-        (0, "Select Gender"),
-        (1, "Female"),
-        (2, "Male"),
-        (3, "Non-Binary"),
+        ("Select", "Select"),
+        ("Female", "Female"),
+        ("Male", "Male"),
+        ("Non-Binary", "Non-Binary"),
     )
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
     email = models.EmailField()
-    relationship = models.CharField(max_length=16, choices=RELATIONSHIP, default=0)
-    gender = models.CharField(max_length=10, choices=GENDER, default=0)
+    relationship = models.CharField(
+        max_length=16, choices=RELATIONSHIP, default=SELECT)
+    gender = models.CharField(max_length=10, choices=GENDER, default=SELECT)
     notes = models.TextField()
     user_id = models.ForeignKey(
         User,
@@ -49,7 +50,7 @@ class Recipient(models.Model):
     )
 
     def __str__(self):
-        return f"{self.name} : {self.relationship}"
+        return f"{self.first_name} : {self.relationship}"
 
     class Meta:
         ordering = ["last_name"]
