ad2ddcf78342160abaffa8c6482241c5fe549ae0
Heather Gray <heathergray153@gmail.com>
2022-06-05 15:48:55 -0600

    updated sidebar with gifts link and added url

--- gift_app/urls.py
+++ gift_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipients/', views.recipient_list, name='recipients'),
+    path('gifts/', views.gift_list, name='gifts'),
 ]
