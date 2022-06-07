5797923c128c82b739d5432ff7b3fcf86c82702a
Heather Gray <heathergray153@gmail.com>
2022-06-06 22:05:54 -0600

    small updates

--- gift_assist/urls.py
+++ gift_assist/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('', include('gift_app.urls')),
