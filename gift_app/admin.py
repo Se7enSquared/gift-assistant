b96d0f7842d47be9a1c6afcd18231fcf8e3aa5c0
Heather Gray <heathergray153@gmail.com>
2022-06-06 22:07:52 -0600

    register additional models to fix #29

--- gift_app/admin.py
+++ gift_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Recipient
+from .models import Recipient, Gift, Occasion
 
 
 admin.site.register(Recipient)
+admin.site.register(Gift)
+admin.site.register(Occasion)
