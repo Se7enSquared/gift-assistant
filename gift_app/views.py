9bfe074bbb7705c7c4604d2507ed600938f8537c
Heather Gray <heathergray153@gmail.com>
2022-06-17 21:42:33 -0600

    working CRUD app!

--- gift_app/views.py
+++ gift_app/views.py
@@ -24,13 +24,19 @@ def recipient_list(request):
                   {'recipients': recipients})
 
 
-def recipient_detail(request):
-    pass
+def recipient_view(request, pk):
+    recipient = Recipient.objects.get(pk=pk)
+    form = RecipientForm(request.GET)
+    return render(request, 'recipients/recipient_view.html', {'recipient': recipient, 'form': form})
 
 
 def recipient_add(request):
+    recipients = Recipient.objects.filter(user=request.user)
+    occasions = Occasion.objects.filter(user=request.user)
     if request.method == "POST":
         form = RecipientForm(request.POST)
+        context = {'form': form, 'recipients': recipients,
+                   'occasions': occasions}
         if form.is_valid():
             recipient = form.save(commit=False)
             recipient.user = request.user
@@ -39,17 +45,38 @@ def recipient_add(request):
                                 headers={'HX-Trigger': 'recipientListChanged'})
     else:
         form = RecipientForm()
-    return render(request, 'recipients/recipient_form.html', {
-        'form': form,
-    })
+        context = {'form': form, 'recipients': recipients,
+                   'occasions': occasions}
+
+    return render(request, 'recipients/recipient_form.html', context)
 
 
-def recipient_edit(request):
-    pass
+def recipient_edit(request, pk):
+    recipient = get_object_or_404(Recipient, pk=pk)
+    if request.method == "POST":
+        form = RecipientForm(request.POST, instance=recipient)
+        context = {'form': form}
+        if form.is_valid():
+            recipient = form.save(commit=False)
+            recipient.user = request.user
+            recipient.save()
+            return HttpResponse(status=204,
+                                headers={'HX-Trigger': 'recipientListChanged'})
+    else:
+        form = RecipientForm(instance=recipient)
+        context = {'form': form}
+    return render(request, 'recipients/recipient_edit.html', context)
 
 
-def recipient_delete(request):
-    pass
+def recipient_delete(request, pk):
+    recipient = get_object_or_404(Recipient, pk=pk)
+
+    if request.method == 'POST':
+        recipient.delete()
+        return HttpResponse(status=204,
+                            headers={'HX-Trigger': 'recipientListChanged'})
+    else:
+        return render(request, 'recipients/recipient_delete.html', {'recipient': recipient})
 
 
 # O C C A S I O N  V I E W S ------------------------>
@@ -58,31 +85,66 @@ def occasions(request):
 
 
 def occasion_list(request):
+    occasions = Occasion.objects.filter(user=request.user)
     return render(request, 'occasions/occasion_list.html',
-                  {'occasions': Occasion.objects.all()})
+                  {'occasions': occasions})
+
+
+def occasion_view(request, pk):
+    occasion = Occasion.objects.get(pk=pk)
+    form = OccasionForm(request.GET)
+    return render(request, 'occasions/occasion_view.html',
+                 {'occasion': occasion, 'form': form})
 
 
 def occasion_add(request):
+    occasions = Occasion.objects.filter(user=request.user)
+    occasions = Occasion.objects.filter(user=request.user)
     if request.method == "POST":
         form = OccasionForm(request.POST)
+        context = {'form': form, 'occasions': occasions,
+                   'occasions': occasions}
         if form.is_valid():
-            form.save()
+            occasion = form.save(commit=False)
+            occasion.user = request.user
+            occasion.save()
             return HttpResponse(status=204,
                                 headers={'HX-Trigger': 'occasionListChanged'})
     else:
         form = OccasionForm()
-    return render(request, 'occasions/occasion_form.html', {
-        'form': form,
-    })
+        context = {'form': form, 'occasions': occasions,
+                   'occasions': occasions}
+
+    return render(request, 'occasions/occasion_form.html', context)
 
 
-def occasion_edit(request):
-    pass
+def occasion_edit(request, pk):
+    occasion = get_object_or_404(Occasion, pk=pk)
+    if request.method == "POST":
+        form = OccasionForm(request.POST, instance=occasion)
+        context = {'form': form}
+        if form.is_valid():
+            occasion = form.save(commit=False)
+            occasion.user = request.user
+            occasion.save()
+            return HttpResponse(status=204,
+                                headers={'HX-Trigger': 'occasionListChanged'})
+    else:
+        form = OccasionForm(instance=occasion)
+        context = {'form': form}
+    return render(request, 'occasions/occasion_edit.html', context)
 
 
-def occasion_delete(request):
-    pass
+def occasion_delete(request, pk):
+    occasion = get_object_or_404(Occasion, pk=pk)
 
+    if request.method == 'POST':
+        occasion.delete()
+        return HttpResponse(status=204,
+                            headers={'HX-Trigger': 'occasionListChanged'})
+    else:
+        return render(request, 'occasions/occasion_delete.html',
+                     {'occasion': occasion})
 
 # G I F T  V I E W S ------------------------>
 def gifts(request):
