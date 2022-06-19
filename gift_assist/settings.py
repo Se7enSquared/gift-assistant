098009472ae02506a5b172b54cee935aeb317c3a
Heather Gray <heathergray153@gmail.com>
2022-06-18 18:50:47 -0600

    cleanup login

--- gift_assist/settings.py
+++ gift_assist/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
+ACCOUNT_LOGOUT_ON_GET = True
 
 # Application definition
 
