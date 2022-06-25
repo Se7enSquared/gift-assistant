baefe62ee8dfdc924cf81c171e4c7d14f5651eff
Heather Gray <heathergray153@gmail.com>
2022-06-25 13:34:58 -0600

    refactor occasions

--- gift_app/occasions.py
+++ gift_app/occasions.py
@@ -1,5 +1,5 @@
+from dataclasses import dataclass
 from datetime import date, timedelta
-import calendar
 
 from .models import Occasion
 
@@ -9,56 +9,79 @@ PARENT = 'Parent'
 MALE = 'Male'
 FEMALE = 'Female'
 CURRENT_YEAR = date.today().year
+CURRENT_MONTH = date.today().month
 SUNDAY = 6
 MAY = 5
 JUNE = 6
 SECOND = 2
 THIRD = 3
 FIRST = 1
+DAYS_IN_WEEK = 7
+
+
+@dataclass
+class IrregularHoliday:
+    '''a dataclass for holidays that are not on a
+        specific day of the month'''
+    name: str
+    month: int
+    year: int
+    weekday: int
+    nth_day: int
+
+
+def get_nth_weekday(holiday: IrregularHoliday) -> date:
+    '''returns the date of the nth weekday of the month'''
+    if CURRENT_MONTH > holiday.month:
+        holiday.year += 1
+
+    first_of_the_month = date(
+        holiday.year, holiday.month, FIRST)
+    first_weekday = first_of_the_month.weekday()
+    if first_weekday != holiday.weekday:
+        days_to_add = 6 - first_weekday
+        first_weekday_date = (first_of_the_month +
+                              timedelta(days=days_to_add))
+    else:
+        first_weekday_date = first_of_the_month
+
+    days_left_to_find = holiday.nth_day - 1
+    return first_weekday_date + timedelta(
+        days=DAYS_IN_WEEK * (days_left_to_find))
 
 
 class AutomateOccasions():
     def __init__(self, recipient: object):
         self.recipient = recipient
 
-    def get_nth_weekday(self, year: int, n: int, weekday: int, month: int) -> date:
-        first_of_the_month = date(year, month, FIRST)
-        first_weekday = first_of_the_month.weekday()
-        if first_weekday != weekday:
-            days_to_add = 6 - first_weekday
-            first_weekday_date = (first_of_the_month +
-                                  timedelta(days=days_to_add))
-        else:
-            first_weekday_date = first_of_the_month
-
-        return first_weekday_date + timedelta(days=7 * (n - 1))
-
     def get_upcoming_date(self, holiday: str) -> date:
         '''Get the next date for the given holiday'''
 
         if holiday == MOTHERS_DAY:
-            mothers_day_date = self.get_nth_weekday(
-                CURRENT_YEAR, SECOND, SUNDAY, MAY)
-
-            # if holiday is in the past get the next year's date
-            if mothers_day_date < date.today():
-                return self.get_nth_weekday(
-                    CURRENT_YEAR + 1, SECOND, SUNDAY, MAY)
-            else:
-                return self.get_nth_weekday(
-                    CURRENT_YEAR, SECOND, SUNDAY, MAY)
-
+            return self._find_irregular_holiday(MOTHERS_DAY, MAY, SECOND)
         if holiday == FATHERS_DAY:
-            fathers_day_date = self.get_nth_weekday(
-                CURRENT_YEAR, THIRD, SUNDAY, JUNE)
+            return self._find_irregular_holiday(FATHERS_DAY, JUNE, THIRD)
 
-            # if holiday is in the past get the next year's date
-            if fathers_day_date < date.today():
-                return self.get_nth_weekday(
-                    CURRENT_YEAR + 1, THIRD, SUNDAY, JUNE)
-            else:
-                return self.get_nth_weekday(
-                    CURRENT_YEAR, THIRD, SUNDAY, JUNE)
+    def _find_irregular_holiday(self, name, month, nth_day):
+        '''builds an irregular holiday object and finds the date of the
+            next occurrence'''
+        holiday = IrregularHoliday(
+            name=name,
+            month=month,
+            year=CURRENT_YEAR,
+            weekday=SUNDAY,
+            nth_day=nth_day
+        )
+
+        holiday_date = get_nth_weekday(holiday)
+
+        if holiday_date > date.today():
+            return holiday_date
+
+        # only in the case that the holiday is in the current month
+        # this code may run
+        holiday.year += 1
+        return get_nth_weekday(holiday)
 
     def auto_add_occasion(self, holiday: str):
         '''Add an occasion for the given holiday'''
