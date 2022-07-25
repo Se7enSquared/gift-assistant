811786860cad56a5a9fdcb445a8010670ec0b3ed
Bob Belderbos <bobbelderbos@gmail.com>
2022-07-25 21:30:29 +0200

    show year only if checkbox clicked https://stackoverflow.com/a/18307407

--- gift_app/static/js/dialog.js
+++ gift_app/static/js/dialog.js
@@ -5,6 +5,17 @@ htmx.on("htmx:afterSwap", (e) => {
   // Response targeting #dialog => show the modal
   if (e.detail.target.id == "dialog") {
     modal.show()
+
+    let birthYearWrapper = $('#id_birth_year').parent().parent();
+    birthYearWrapper.hide();
+
+    $("#id_birth_year_unknown").click(function() {
+      if($(this).is(":checked")) {
+          birthYearWrapper.show();
+      } else {
+          birthYearWrapper.hide();
+      }
+    });
   }
 })
 
