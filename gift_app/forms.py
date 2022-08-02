d9bd45523bf8fd24b36dcf5e7575599f5a9ff6a6
Heather Gray <heathergray153@gmail.com>
2022-08-01 20:13:00 -0600

    WIP WILL NOT KEEP: Experimenting with form validation

--- gift_app/forms.py
+++ gift_app/forms.py
@@ -1,6 +1,8 @@
+from datetime import date
+import calendar
 from django import forms
 from django.forms import ModelForm
-from .models import Occasion, Recipient, Gift
+from .models import Occasion, Recipient, Gift, CURRENT_YEAR
 
 
 class RecipientForm(ModelForm):
@@ -9,13 +11,53 @@ class RecipientForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['age'].help_text = '(approximate age if unknown)'
 
+    # BOB: There are multiple issues with this code... Mainly -
+    # the ValueError from the day being the wrong value (for exmaple "43")
+    # is actually raised when calculate_age endpoint is called.
+    # I want to validate the birth day value as soon as its entered, but
+    # if the month is februrary, I can't know that until the year is added
+    # So I think I wasted my time with this code because the date time module
+    # should take care of it, which I knew but I was trying different things
+    # I'm just leaving this code here for tonight to show what I was doing
+    # Even though it is wrong
+    # Next todo: figure out how to raise an error to a form through a view!
+    # I am out of time so can't research this tonight
+    @staticmethod
+    def _validate_day(clean_data):
+        month = clean_data['birth_month']
+        day = clean_data['birth_day']
+        year = clean_data['birth_year']
+
+        if month == 2:
+            return day <= 29 if calendar.isleap(year) else day <= 28
+        if month in (4, 6, 9, 11):
+            return day <= 30
+        return day <= 31
+
+    @staticmethod
+    def _create_birthday(clean_data):
+        day = clean_data['birth_day']
+        month = clean_data['birth_month']
+        year = clean_data['birth_year']
+        try:
+            return date(year, month, day)
+        except ValueError:
+            return False
+
+    # here I removed your call to super().clean() or whatever it was before
+    # because I was experimenting again. I had noticed that I was able to get
+    # the data without that and just using self.cleaned_data. Let me know if
+    # that was wrong. Thanks!
     def clean(self):
-        cleaned_data = super().clean()
-        if cleaned_data["birth_day"] is not None:
-            bday = int(cleaned_data["birth_day"])
-            # TODO: use datetime because Feb has 28 (or 29) days
-            if bday < 1 or bday > 31:
-                raise forms.ValidationError("birth day is wrong")
+        if not self._create_birthday(self.cleaned_data):
+            raise forms.ValidationError('Invalid Birthday')
+
+    # again you can see here I was experimenting. I learned
+    # the difference between form level and field level validation
+    def clean_birth_day(self):
+        if not self._validate_day(self.cleaned_data):
+            raise forms.ValidationError('Invalid day')
+        return
 
     class Meta:
         model = Recipient
